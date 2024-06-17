20218591c2c750813ef649af93329a40517dd43f
Your Name <you@example.com>
2024-06-17 16:58:02 +0300

    Добавил картинку на сайт

--- internetshop/shop/views.py
+++ internetshop/shop/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hi! This is an internet shop")
+    return HttpResponse("<h1>Internet site</h1> <img src='https://archive.unews.utah.edu/wp-content/uploads/2021/03/Cash.jpg'> Some description")
 
