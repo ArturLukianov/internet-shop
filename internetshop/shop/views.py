a166733e24ed7645d89df8aef927df8752a23814
Your Name <you@example.com>
2024-06-20 16:32:50 +0300

    Перенёс код в index.html

--- internetshop/shop/views.py
+++ internetshop/shop/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>Internet site</h1> <img src='https://archive.unews.utah.edu/wp-content/uploads/2021/03/Cash.jpg'> Some description")
+    return render(request, "index.html")
 
