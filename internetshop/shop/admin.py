afeb4bc014aceeec19a3d3678a52fb987fee02f1
Your Name <you@example.com>
2024-06-24 17:24:17 +0300

    Добавлена админка для продуктов

--- internetshop/shop/admin.py
+++ internetshop/shop/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from . import models
+
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
 # Register your models here.
+admin.site.register(models.Product, ProductAdmin)
--- internetshop/shop/models.py
+++ internetshop/shop/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.CharField(max_length=128)
+    price = models.IntegerField()
+    image_url = models.CharField(max_length=256)
