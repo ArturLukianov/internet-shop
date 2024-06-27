3bd5c4ad813bb06b0803fc04a97b5b03482d620f
Your Name <you@example.com>
2024-06-27 16:46:42 +0300

    Добавлена админка для продуктов

--- internetshop/shop/models.py
+++ internetshop/shop/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=128)
-    description = models.CharField(max_length=128)
+    description = models.TextField()
     price = models.IntegerField()
     image_url = models.CharField(max_length=256)
