f06e93d710ff44b37bb27ecb6bc21ea1c96ec4b0
Your Name <you@example.com>
2024-07-01 17:27:51 +0300

    Добавил отображение характеристик

--- internetshop/shop/models.py
+++ internetshop/shop/models.py
@@ -7,5 +7,14 @@ class Product(models.Model):
     price = models.IntegerField()
     image_url = models.CharField(max_length=256)
 
+    # Вес в граммах
+    weight = models.IntegerField(null=True, blank=True)
+
+    model = models.CharField(max_length=64, null=True, blank=True)
+    processor = models.CharField(max_length=32, null=True, blank=True)
+    ram_size = models.IntegerField(null=True, blank=True)  # размер оперативной памяти
+    speed = models.FloatField(null=True, blank=True)  # скорость
+    battery_capacity = models.IntegerField(null=True, blank=True)  # ёмкость аккумулятора
+
     def __str__(self):
         return f'{self.name}'
--- internetshop/shop/views.py
+++ internetshop/shop/views.py
@@ -12,6 +12,7 @@ def home(request):
 
 def view_product(request, id):
     product = Product.objects.filter(id=id).first()
+
     return render(request, 'product.html', {
         'product': product
     })
